014cc2984f62b4db36dc36b901eb86d2c1d33848
Araya <joaoaraya7@gmail.com>
2025-08-02 19:36:47 -0300

    Style: story and gallery stylized

--- app/components/homeSession/Contact/index.tsx
+++ app/components/homeSession/Contact/index.tsx
@@ -4,38 +4,42 @@ export default function Contact() {
 
     return (
         <div id='session-contact'>
-            <div id='contact-form'>
-                <h1>Reserve Your EV Today!</h1>
+            <div id='contact-outside-box'>
+                <div id='contact-inside-box'>
+                    <div id='contact-form'>
+                        <h1>Reserve Your EV Today!</h1>
 
-                <form action='/' method='post'>
-                    <div id='form-first-name'>
-                        <label htmlFor='firstName'>First Name</label>
-                        <input type='text' id='firstName' placeholder='John' />
+                        <form action='/' method='post'>
+                            <div id='form-first-name'>
+                                <label htmlFor='firstName'>First Name</label>
+                                <input type='text' id='firstName' placeholder='John' />
+                            </div>
+
+                            <div id='form-last-name'>
+                                <label htmlFor='lastName'>Last Name</label>
+                                <input type='text' id='lastName' placeholder='Doe' />
+                            </div>
+
+                            <div id='form-mail'>
+                                <label htmlFor='mail'>Mail</label>
+                                <input type='text' id='mail' placeholder='name@mail.com' />
+                            </div>
+
+                            <div id='form-message'>
+                                <label htmlFor='message'>Message</label>
+                                <textarea name="textarea_name" id='message' placeholder='Message' />
+                            </div>
+
+                            <div id='button-send'>
+                                <input type='submit' value='Book Now' />
+                            </div>
+                        </form>
                     </div>
 
-                    <div id='form-last-name'>
-                        <label htmlFor='lastName'>Last Name</label>
-                        <input type='text' id='lastName' placeholder='Doe' />
+                    <div id='contact-banner'>
+                        <img src='/assets/img-contact-car.png' alt='' />
                     </div>
-
-                    <div id='form-mail'>
-                        <label htmlFor='mail'>Mail</label>
-                        <input type='text' id='mail' placeholder='name@mail.com' />
-                    </div>
-
-                    <div id='form-message'>
-                        <label htmlFor='message'>Message</label>
-                        <textarea name="textarea_name" id='message' placeholder='Message' />
-                    </div>
-
-                    <div id='button-send'>
-                        <input type='submit' value='Book Now' />
-                    </div>
-                </form>
-            </div>
-
-            <div id='contact-banner'>
-                <img src='/assets/img-contact-car.png' alt='' />
+                </div>
             </div>
         </div>
     )
--- app/components/homeSession/Gallery/index.tsx
+++ app/components/homeSession/Gallery/index.tsx
@@ -1,18 +1,36 @@
 import './style.scss';
 
 export default function Gallery() {
+    const imagesGallery = [
+        {
+            path: 'img-galery-inside-1.png',
+            describle: 'inside'
+        }, {
+            path: 'img-galery-inside-2.png',
+            describle: 'inside'
+        }, {
+            path: 'img-galery-inside-3.png',
+            describle: 'inside'
+        }
+    ]
 
     return (
         <div id='session-gallery'>
-            <div>
+            <div id='gallery-header'>
                 <h1>Overview</h1>
                 <h2>Future-Ready Comfort</h2>
             </div>
 
-            <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
+            <div id='gallery-media'>
+                <div id='media-scroller'>
+                    {imagesGallery.map((img, index) =>
+                        <img key={index} src={'/assets/' + img.path} alt={img.describle} />
+                    )}
+                    {/*EFEITO INFINITO*/}
+                    {imagesGallery.map((img, index) =>
+                        <img key={index} src={'/assets/' + img.path} alt={img.describle} />
+                    )}
+                </div>
             </div>
         </div>
     )
