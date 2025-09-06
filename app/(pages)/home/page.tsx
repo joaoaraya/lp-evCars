08f91015d5b86c405fdcaf03661a4f0ccd019cf5
Araya <joaoaraya7@gmail.com>
2025-09-06 20:29:37 -0300

    Style: new mobile layout for Nav, header and Energy sessions

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -12,10 +12,8 @@ export default function Home() {
 
     return (
         <div id='home'>
-            {
-                /*<NavBar />
-                <Header />*/
-            }
+            <NavBar />
+            <Header />
 
             <Chargers />
             <Story />
